0963646172fa25623d9d038b9047992fe3b3128a
Marko Kotar <kotarmarko@yahoo.com>
2014-10-31 22:23:44 +0100

    Revert "Undo commit"
    This reverts commit cd72276ab6e8170384596cef3a1a1641defcc736.

--- board.js
+++ board.js
@@ -1,7 +1,6 @@
-BoardObj = function() {
-    this.type="BoardObj";
-    this.fields = [];
-    var color = false;
+Board = {
+  initialize: function() {
+    color = false;
     for (var i = 0; i < 8; i++) {
       this.fields.push([]);
       for (var j = 0; j < 8; j++) {
@@ -10,42 +9,11 @@ BoardObj = function() {
         color = !color;
       }
       color = !color;
-
     }
-    return this;
+  },
+  fields: []
 };
-
-//Boards = new Mongo.Collection("boards");
-//Board = Boards.findOne();
-//if (Board === null||Board === undefined) {
-
-//  Boards.insert(Board);
-//}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    Board = new BoardObj();
-    Board = extendObject(Board);
-  });
-}
-
-
-function extendObject(obj)
-{
-  console.log(obj);
-  if(typeof obj.type != 'undefined')
-  {
-    for(var prop in obj)
-    {
-      if(typeof obj[prop] == 'object')
-        obj[prop] = extendObject(obj[prop]);
-    }
-    if(obj.type.match(/^[a-zA-Z0-9_]+$/g))
-      return _.extend(obj, eval(obj.type + ".prototype"));
-    else
-      return obj;
-  }
-}
+Board.initialize();
 
 if(Meteor.isServer)
 {
--- chess.js
+++ chess.js
@@ -1,4 +1,4 @@
-
+Board
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
@@ -19,7 +19,7 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function() {
     // code to run on server at startup
-
+    
   });
 
 }
